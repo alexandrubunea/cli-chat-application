c336c6514ee2a9320cb3659e33862e0ca06e61a3
Bunea Alexandru <alexandrubunea03@gmail.com>
2024-06-01 01:34:24 +0300

    Update Added connection between multiple clients and the server. Added Diffie-Hellman key exchange between a client and the server.

--- Client/Client.py
+++ Client/Client.py
@@ -3,12 +3,84 @@ Client program for the CLI Chat Application
 
 Author: Bunea Alexandru
 """
+import secrets
+import socket
+
+from Security.DHKeys import P_KEY, G_KEY
 
 
 class Client:
-    def __init__(self, host: str, port: int):
+    def __init__(self, host: str, host_port: int, client_port: int = 1713):
+        """
+        Initialize the client.
+        :param host: Host to connect.
+        :param host_port: Port of the host.
+        :param client_port: Client port which will be used later for messaging.
+        """
+
         # Assign args
         self.host = host
-        self.port = port
+        self.host_port = host_port
+        self.client_port = client_port
 
-        print("Client initialized.")
+        # Diffie-Hellman
+        self.private_key = secrets.randbits(4096)  # Generates a number for the secret key
+        self.public_key = pow(G_KEY, self.private_key, P_KEY)  # Calculate the public key
+
+    def start(self) -> None:
+        """
+        Starts the client application.
+        :return: None
+        """
+        print("__          __  _                            _ \n"
+              "\ \        / / | |                          | |       \n"
+              " \ \  /\  / /__| | ___ ___  _ __ ___   ___  | |_ ___  \n"
+              "  \ \/  \/ / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | __/ _ \ \n"
+              "   \  /\  /  __/ | (_| (_) | | | | | |  __/ | || (_) |\n"
+              "    \/  \/ \___|_|\___\___/|_| |_| |_|\___|  \__\___/ \n")
+        print("   _____                           ________          __  ___              \n"
+              "  / ___/___  _______  __________  / ____/ /_  ____ _/ /_/   |  ____  ____ \n"
+              "  \__ \/ _ \/ ___/ / / / ___/ _ \/ /   / __ \/ __ `/ __/ /| | / __ \/ __ \ \n"
+              " ___/ /  __/ /__/ /_/ / /  /  __/ /___/ / / / /_/ / /_/ ___ |/ /_/ / /_/ /\n"
+              "/____/\___/\___/\__,_/_/   \___/\____/_/ /_/\__,_/\__/_/  |_/ .___/ .___/ \n"
+              "                                                           /_/   /_/      \n")
+        print(f"* Trying to connect you to the host [{self.host}:{self.host_port}]")
+        self.__check_host_availability__()
+        self.__establish_connection__()
+
+    def __establish_connection__(self):
+        try:
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.connect((self.host, self.host_port))
+
+                # Sharing keys
+                packed_key = self.public_key.to_bytes(
+                    513,
+                    byteorder="big"
+                )
+                s.sendall(packed_key)
+                raw_data = s.recv(513)
+                server_public_key = int.from_bytes(raw_data, byteorder="big")
+                secret_key = pow(server_public_key, self.private_key, P_KEY)  # Using Diffie-Hellman
+
+                s.close()
+
+        except (socket.timeout, socket.error) as e:
+            print(f"* Connection ended: {e}")
+
+    def __check_host_availability__(self) -> bool:
+        """
+        Checks if the host is up and running on the specified port.
+        :return: true if the server is up, false otherwise
+        """
+        try:
+            with socket.create_connection((self.host, self.host_port), 5):
+                return True
+        except (socket.timeout, socket.error) as e:
+            print(f"* Failed to connect to {self.host}:{self.host_port}: {e}")
+            return False
+
+
+if __name__ == "__main__":
+    client = Client("127.0.0.1", 1712, 1713)
+    client.start()
--- Server/Server.py
+++ Server/Server.py
@@ -3,11 +3,87 @@ Server program for the CLI Chat Application
 
 Author: Bunea Alexandru
 """
+import asyncio
+import secrets
+
+from Security.DHKeys import P_KEY, G_KEY
 
 
 class Server:
-    def __init__(self, port: int):
-        # Assign args
-        self.port = port
+    def __init__(self, host: str = "127.0.0.1", port: int = 1712, max_clients_allowed: int = 100, debug: bool = True):
+        """
+        Initialize the server.
+        :param host: Host of the server.
+        :param port: Port of the server.
+        :param max_clients_allowed: Maximum number of clients allowed to connect to the server.
+        :param debug: If it's true the server will print to the console different debugging information.
+        """
 
-        print("The server was initialized")
+        # Assign args
+        self.host = host
+        self.port = port
+        self.max_clients_allowed = max_clients_allowed
+        self.debug = debug
+
+        # Diffie-Hellman
+        self.private_key = secrets.randbits(4096)  # Generates a number for the secret key
+        self.public_key = pow(G_KEY, self.private_key, P_KEY)  # Calculate the public key
+
+        self.__print_debug__("The server was initialized.")
+        self.__print_debug__(f"Will run on {host}:{port}")
+        self.__print_debug__(f"Allowing a maximum number of {max_clients_allowed} clients to connect.")
+
+    def start(self) -> None:
+        """
+        Starts the server.
+        :return: None
+        """
+        asyncio.run(self.__run_server__())
+
+    async def __run_server__(self):
+        """
+        Stars a server instance, so multiple clients can connect
+        :return:
+        """
+        self.__print_debug__("Starting a server instance on asyncio...")
+        server = await asyncio.start_server(self.__handle_client__, self.host, self.port)
+
+        # Gets all the sockets where the server is running.
+        addrs = ", ".join(str(sock.getsockname()) for sock in server.sockets)
+        self.__print_debug__(f"Serving on {addrs}")
+
+        async with server:
+            await server.serve_forever()
+
+    async def __handle_client__(self, reader: asyncio.StreamReader, writer: asyncio.StreamWriter) -> None:
+        raw_data = await reader.read(513)  # It's not a magic number, it's from the size of P_KEY which is 4096-bit
+        client_public_key = int.from_bytes(raw_data, byteorder="big")
+
+        if not client_public_key:  # This mean it was just an availability check, connection can be dropped
+            return
+
+        packed_key = self.public_key.to_bytes(
+            513,
+            byteorder="big"
+        )
+        writer.write(packed_key)
+        await writer.drain()
+
+        secret_key = pow(client_public_key, self.private_key, P_KEY)  # Using Diffie-Hellman
+
+        writer.close()
+        await writer.wait_closed()
+
+    def __print_debug__(self, msg: str) -> None:
+        """
+        Prints a string to the console as a debug message
+        :param msg: The message to print
+        :return: None
+        """
+        if self.debug:
+            print(f"[DEBUG]: {msg}")
+
+
+if __name__ == "__main__":
+    zerver = Server()
+    zerver.start()
